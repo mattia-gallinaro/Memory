280b8d9137a21f487bf11abc97d87739b6147896
mattia-gallinaro <gallinaromattia6@gmail.com>
2021-09-23 09:51:07 +0200

    Aggiunta carte

--- Memory/Form1.Designer.cs
+++ Memory/Form1.Designer.cs
@@ -29,13 +29,128 @@ namespace Memory
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.carta1 = new System.Windows.Forms.PictureBox();
+            this.carta2 = new System.Windows.Forms.PictureBox();
+            this.carta3 = new System.Windows.Forms.PictureBox();
+            this.carta4 = new System.Windows.Forms.PictureBox();
+            this.carta5 = new System.Windows.Forms.PictureBox();
+            this.carta6 = new System.Windows.Forms.PictureBox();
+            this.carta7 = new System.Windows.Forms.PictureBox();
+            this.carta8 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.carta1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // carta1
+            // 
+            this.carta1.Location = new System.Drawing.Point(22, 60);
+            this.carta1.Name = "carta1";
+            this.carta1.Size = new System.Drawing.Size(153, 188);
+            this.carta1.TabIndex = 0;
+            this.carta1.TabStop = false;
+            // 
+            // carta2
+            // 
+            this.carta2.Location = new System.Drawing.Point(218, 60);
+            this.carta2.Name = "carta2";
+            this.carta2.Size = new System.Drawing.Size(153, 188);
+            this.carta2.TabIndex = 1;
+            this.carta2.TabStop = false;
+            // 
+            // carta3
+            // 
+            this.carta3.Location = new System.Drawing.Point(417, 60);
+            this.carta3.Name = "carta3";
+            this.carta3.Size = new System.Drawing.Size(153, 188);
+            this.carta3.TabIndex = 2;
+            this.carta3.TabStop = false;
+            // 
+            // carta4
+            // 
+            this.carta4.Location = new System.Drawing.Point(602, 60);
+            this.carta4.Name = "carta4";
+            this.carta4.Size = new System.Drawing.Size(153, 188);
+            this.carta4.TabIndex = 3;
+            this.carta4.TabStop = false;
+            // 
+            // carta5
+            // 
+            this.carta5.Location = new System.Drawing.Point(22, 276);
+            this.carta5.Name = "carta5";
+            this.carta5.Size = new System.Drawing.Size(153, 188);
+            this.carta5.TabIndex = 4;
+            this.carta5.TabStop = false;
+            // 
+            // carta6
+            // 
+            this.carta6.Location = new System.Drawing.Point(218, 276);
+            this.carta6.Name = "carta6";
+            this.carta6.Size = new System.Drawing.Size(153, 188);
+            this.carta6.TabIndex = 5;
+            this.carta6.TabStop = false;
+            // 
+            // carta7
+            // 
+            this.carta7.Location = new System.Drawing.Point(417, 276);
+            this.carta7.Name = "carta7";
+            this.carta7.Size = new System.Drawing.Size(153, 188);
+            this.carta7.TabIndex = 6;
+            this.carta7.TabStop = false;
+            // 
+            // carta8
+            // 
+            this.carta8.Location = new System.Drawing.Point(602, 276);
+            this.carta8.Name = "carta8";
+            this.carta8.Size = new System.Drawing.Size(153, 188);
+            this.carta8.TabIndex = 7;
+            this.carta8.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.carta8);
+            this.Controls.Add(this.carta7);
+            this.Controls.Add(this.carta6);
+            this.Controls.Add(this.carta5);
+            this.Controls.Add(this.carta4);
+            this.Controls.Add(this.carta3);
+            this.Controls.Add(this.carta2);
+            this.Controls.Add(this.carta1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.carta1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta8)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox carta1;
+        private System.Windows.Forms.PictureBox carta2;
+        private System.Windows.Forms.PictureBox carta3;
+        private System.Windows.Forms.PictureBox carta4;
+        private System.Windows.Forms.PictureBox carta5;
+        private System.Windows.Forms.PictureBox carta6;
+        private System.Windows.Forms.PictureBox carta7;
+        private System.Windows.Forms.PictureBox carta8;
     }
 }
 
